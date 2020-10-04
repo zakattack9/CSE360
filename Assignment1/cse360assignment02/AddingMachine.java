50a4c59c850636b3a41603919924d6d7d67d8ecc
zakattack9 <zakattack26111@gmail.com>
2020-10-03 16:15:14 -1000

    added comments

--- Assignment1/cse360assignment02/AddingMachine.java
+++ Assignment1/cse360assignment02/AddingMachine.java
@@ -12,29 +12,51 @@ public class AddingMachine {
     private int total;
     private ArrayList<String> history = new ArrayList<>();
 
+    /* AddingMachine Constructor */
     public AddingMachine () {
         total = 0;
         history.add("0");
     }
 
+    /**
+     * Returns the current sum of then numbers added or subtracted
+     *
+     * @return current total
+     */
     public int getTotal () {
         return total;
     }
 
+    /**
+     * Adds a number to the current sum
+     *
+     * @param integer to add
+     */
     public void add (int value) {
         total += value;
         history.add(" + " + value);
     }
 
+    /**
+     * Subtracts a number from the current sum
+     *
+     * @param integer to subtract
+     */
     public void subtract (int value) {
         total -= value;
         history.add(" - " + value);
     }
 
+    /**
+     * Returns the history of all operations performed on the current total
+     *
+     * @return string containing the history of operations
+     */
     public String toString () {
         return String.join("", history);
     }
 
+    /* Resets the total and clears the current history of operations */
     public void clear() {
         total = 0;
         history.clear();
