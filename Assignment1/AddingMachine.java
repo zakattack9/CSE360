ad0e1f544c5e440af906f280eb93c8ada2df2bb8
zakattack9 <zakattack26111@gmail.com>
2020-10-03 15:08:07 -1000

    adding assignment 1

---
+++ Assignment1/AddingMachine.java
@@ -0,0 +1,32 @@
+package cse360assignment02;
+
+/**
+ * This class adds and subtracts values from a sum.
+ * CSE360 Assignment 1
+ * Zak Sakata
+ */
+
+public class AddingMachine {
+    private int total;
+
+    public AddingMachine () {
+        total = 0;  // not needed - included for clarity
+    }
+
+    public int getTotal () {
+        return 0;
+    }
+
+    public void add (int value) {
+    }
+
+    public void subtract (int value) {
+    }
+
+    public String toString () {
+        return "";
+    }
+
+    public void clear() {
+    }
+}
