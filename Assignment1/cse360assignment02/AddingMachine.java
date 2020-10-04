6ef39e39efbc165bd23da3b60c5118b532f05f41
zakattack9 <zakattack26111@gmail.com>
2020-10-03 16:01:34 -1000

    updated methods and added main class

--- Assignment1/cse360assignment02/AddingMachine.java
+++ Assignment1/cse360assignment02/AddingMachine.java
@@ -1,5 +1,7 @@
 package cse360assignment02;
 
+import java.util.ArrayList;
+
 /**
  * This class adds and subtracts values from a sum.
  * CSE360 Assignment 1
@@ -8,25 +10,34 @@ package cse360assignment02;
 
 public class AddingMachine {
     private int total;
+    private ArrayList<String> history = new ArrayList<>();
 
     public AddingMachine () {
-        total = 0;  // not needed - included for clarity
+        total = 0;
+        history.add("0");
     }
 
     public int getTotal () {
-        return 0;
+        return total;
     }
 
     public void add (int value) {
+        total += value;
+        history.add(" + " + value);
     }
 
     public void subtract (int value) {
+        total -= value;
+        history.add(" - " + value);
     }
 
     public String toString () {
-        return "";
+        return String.join("", history);
     }
 
     public void clear() {
+        total = 0;
+        history.clear();
     }
 }
+
